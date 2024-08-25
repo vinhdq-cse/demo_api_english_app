98b9137da8f7bd92ad4575051d2753feea7ba7b6
Quang Vinh <vinh.dangquang03@hcmut.edu.vn>
2024-08-25 15:25:23 +0700

    add home view

--- apis/views.py
+++ apis/views.py
@@ -3,7 +3,11 @@
 from rest_framework import generics
 from .models import User
 from .serializers import UserSerializer
+from django.http import HttpResponse
 
 class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+def home(request):
+    return HttpResponse("Hello, world!")
--- ea_apis/urls.py
+++ ea_apis/urls.py
@@ -2,8 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from apis.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('apis.urls')),  # Đảm bảo rằng bạn đã bao gồm URLs của ứng dụng
 ]
